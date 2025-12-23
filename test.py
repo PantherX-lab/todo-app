89c25753f2f5c307f9a63ff9d263b7a04e5a9486
kunlewilliams <kunlew24@gmail.com>
2025-12-23 16:14:37 -0500

    Initial commit

---
+++ test.py
@@ -0,0 +1,9 @@
+def greet(message):
+    new_message = message.capitalize()
+    print("Hey hey")
+    return new_message
+
+user_entry = input("What greeting would you like?")
+greeting = greet(user_entry)
+print(greeting)
+
